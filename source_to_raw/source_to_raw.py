e1bc1f6b4262efac9ae484ae67b0a014b69d1438
Victor Sago <VictorSago01@gmail.com>
2022-07-11 14:21:28 +0200

    get_info returns a python dict now

--- source_to_raw/source_to_raw.py
+++ source_to_raw/source_to_raw.py
@@ -10,5 +10,5 @@ BASE_URL = "https://swapi.dev/api/"
 def get_info(section, searchterm):
     req_url = BASE_URL + section + "/" + searchterm
     resp = requests.get(req_url)
-    return resp.json()
+    return json.loads(resp.text)
 
